40dad9b1087095f10c7d14e776aaf4c9e296aa78
Anthony Nahas <anthony.na@hotmail.de>
2019-01-11 15:38:21 +0100

    chore(project): added generated source code and docs

---
+++ src/app/shell/header/header.component.ts
@@ -0,0 +1,35 @@
+import { Title } from '@angular/platform-browser';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatSidenav } from '@angular/material';
+
+import { I18nService } from '@app/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  @Input() sidenav: MatSidenav;
+
+  constructor(private titleService: Title, private i18nService: I18nService) {}
+
+  ngOnInit() {}
+
+  setLanguage(language: string) {
+    this.i18nService.language = language;
+  }
+
+  get currentLanguage(): string {
+    return this.i18nService.language;
+  }
+
+  get languages(): string[] {
+    return this.i18nService.supportedLanguages;
+  }
+
+  get title(): string {
+    return this.titleService.getTitle();
+  }
+}
