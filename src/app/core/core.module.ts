6412bd8873a6b3d04a9f89169751e32899cc29b6
Anthony Nahas <anthony.na@hotmail.de>
2019-01-27 22:42:57 +0100

    feat(package): added footer component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -10,6 +10,8 @@ import { HttpCacheService } from './http/http-cache.service';
 import { ApiPrefixInterceptor } from './http/api-prefix.interceptor';
 import { ErrorHandlerInterceptor } from './http/error-handler.interceptor';
 import { CacheInterceptor } from './http/cache.interceptor';
+import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @NgModule({
   imports: [CommonModule, HttpClientModule, TranslateModule, RouterModule],
@@ -27,7 +29,8 @@ import { CacheInterceptor } from './http/cache.interceptor';
       provide: RouteReuseStrategy,
       useClass: RouteReusableStrategy
     }
-  ]
+  ],
+  declarations: [HeaderComponent, FooterComponent]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
