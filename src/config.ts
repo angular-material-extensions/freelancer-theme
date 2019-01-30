0d7981f60c8ed090576e62108fc2f38a6cf6e409
Anthony Nahas <anthony.na@hotmail.de>
2019-01-30 18:13:04 +0100

    feat(package): added config file to the project

---
+++ src/config.ts
@@ -0,0 +1,28 @@
+/**
+ * @author Anthony Nahas
+ * @since 01.2019
+ * @version 1.0
+ */
+export interface Config {
+  title: string;
+  description: string;
+  url: string;
+  domain?: string;
+  skills?: string[];
+  techStack?: string[];
+
+  social: {
+    twitterUserName?: string;
+    facebookUserName?: string;
+    githubUserName?: string;
+    linkedInUserName?: string;
+  };
+
+  address: {
+    street?: string;
+    zip?: string;
+    city?: string;
+    state?: string;
+    country?: string;
+  };
+}
