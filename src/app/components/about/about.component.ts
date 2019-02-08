978a212a1f4ab6d9797e7a3de6ff1053f4045e89
Anthony Nahas <anthony.na@hotmail.de>
2019-02-08 16:58:22 +0100

    fix(package): bind the configuration in the about component

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,20 @@
+import { Component, Inject, OnInit } from '@angular/core';
+
+import { environment } from '../../../environments/environment';
+import { Config, ConfigToken } from '../../../config';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+  version: string = environment.version;
+
+  constructor(
+    @Inject(ConfigToken)
+    public config: Config
+  ) {}
+
+  ngOnInit() {}
+}
