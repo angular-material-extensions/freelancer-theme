9bc6d449051208a8a33c54b1ed1e857f67a38c01
Anthony Nahas <anthony.na@hotmail.de>
2019-02-09 17:43:34 +0100

    fix(lint): minor issue

--- src/config.ts
+++ src/config.ts
@@ -51,7 +51,7 @@ export const DEFAULT_CONFIG: Config = {
   about:
     'This Freelancer Theme is a free material theme created by angular material extensions based on Start' +
     ' Bootstrap.The download includes the complete source files including HTML, CSS, and JavaScript as well as ' +
-    "optional LESS stylesheets for easy customization. Whether you're a student looking to showcase your work, a" +
+    'optional LESS stylesheets for easy customization. Whether you are a student looking to showcase your work, a' +
     ' professional looking to attract clients, or a graphic artist looking to share your projects, this template ' +
     'is the perfect starting point!',
   address: {
