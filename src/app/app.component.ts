2f4e408de834340c1cfdaba653b4efbeba0dc68c
Anthony Nahas <anthony.na@hotmail.de>
2019-02-19 22:53:47 +0100

    fix(project): minor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { DomSanitizer, Title } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
@@ -9,6 +9,7 @@ import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 import { environment } from '@env/environment';
 import { I18nService, Logger } from '@app/core';
 import { MatIconRegistry } from '@angular/material';
+import { isPlatformServer } from '@angular/common';
 
 const log = new Logger('App');
 
@@ -19,6 +20,7 @@ const log = new Logger('App');
 })
 export class AppComponent implements OnInit {
   constructor(
+    @Inject(PLATFORM_ID) private platformId: string,
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private titleService: Title,
@@ -30,12 +32,15 @@ export class AppComponent implements OnInit {
     private _iconRegistry: MatIconRegistry,
     private _sanitizer: DomSanitizer
   ) {
+    // domain and port for SSR in this example is static. Use i.e. environment files to use appropriate dev/prod domain:port
+    // const domain = (isPlatformServer(platformId)) ? 'http://localhost:4000' : 'https://angular-material-extensions.github.io/freelancer-theme';
+    const domain = isPlatformServer(platformId) ? 'http://localhost:4000' : '';
     console.log('on construct app component');
     _iconRegistry
-      .addSvgIcon('facebook', _sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/facebook.svg'))
-      .addSvgIcon('twitter', _sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/twitter.svg'))
-      .addSvgIcon('linkedin', _sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/linkedin.svg'))
-      .addSvgIcon('github', _sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/github-circle.svg'));
+      .addSvgIcon('facebook', _sanitizer.bypassSecurityTrustResourceUrl(`${domain}/assets/icons/facebook.svg`))
+      .addSvgIcon('twitter', _sanitizer.bypassSecurityTrustResourceUrl(`${domain}/assets/icons/twitter.svg`))
+      .addSvgIcon('linkedin', _sanitizer.bypassSecurityTrustResourceUrl(`${domain}/assets/icons/linkedin.svg`))
+      .addSvgIcon('github', _sanitizer.bypassSecurityTrustResourceUrl(`${domain}/assets/icons/github-circle.svg`));
   }
 
   ngOnInit() {
